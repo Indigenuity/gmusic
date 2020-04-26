6ebd66e3b5ba2c4410528c2469fb3011d72fbfb7
JD Kemsley <jd@JDs-MacBook-Pro.local>
2020-04-26 12:48:18 -0500

    Pretty print everthing

--- get_info.py
+++ get_info.py
@@ -11,6 +11,7 @@ PLAYLIST_INFO_PATH = os.path.join(STORAGE_PATH, "all_playlists.json")
 PLAYLIST_CONTENT_PATH = os.path.join(STORAGE_PATH, "playlists")
 TRACK_CONTENT_PATH = os.path.join(STORAGE_PATH, "raw_tracks")
 
+# Tablet
 DEVICE_ID = "342e914abacc484d"
 
 print("Logging in")
@@ -21,17 +22,17 @@ report = {}
 print("Getting registered devices")
 with open(DEVICES_PATH, "w") as f:
     registered_devices = api.get_registered_devices()
-    json.dump(registered_devices, f)
+    json.dump(registered_devices, f, indent=2)
 
 print("Getting all songs in library")
 with open(LIBRARY_PATH, "w") as f:
     library = api.get_all_songs()
-    json.dump(library, f)
+    json.dump(library, f, indent=2)
 
 print("Getting all playlists")
 with open(PLAYLIST_INFO_PATH, "w") as f:
     playlists = api.get_all_playlists()
-    json.dump(playlists, f)
+    json.dump(playlists, f, indent=2)
 
 print("Getting playlist contents")
 if not os.path.exists(PLAYLIST_CONTENT_PATH):
@@ -43,23 +44,23 @@ for playlist in playlist_contents:
     playlist_filename = "{}.json".format(os.path.join(PLAYLIST_CONTENT_PATH, playlist_name))
     print("Saving playlist '{}' as {}".format(playlist_name, playlist_filename))
     with open(playlist_filename, "w") as f:
-        json.dump(playlist, f)
+        json.dump(playlist, f, indent=2)
 
-if not os.path.exists(TRACK_CONTENT_PATH):
-    os.makedirs(TRACK_CONTENT_PATH)
-for track in library[:10]:
-    track_title = "".join([x if x.isalnum() else "_" for x in track["title"]])
-    filename = "{}.mp3".format(os.path.join(TRACK_CONTENT_PATH, track_title))
-    print("Downloading {} and saving to {}".format(track['title'], filename))
-    stream_url = api.get_stream_url(track['id'])
-    response = urllib.request.urlopen(stream_url)
-    data = response.read()
-    with open(filename, "wb") as f:
-        f.write(data)
-    if 'storeId' in track:
-        print("has store id : {}".format(track['title']))
-    else:
-        print("no store id : {}".format(track['title']))
+# if not os.path.exists(TRACK_CONTENT_PATH):
+#     os.makedirs(TRACK_CONTENT_PATH)
+# for track in library[:10]:
+#     track_title = "".join([x if x.isalnum() else "_" for x in track["title"]])
+#     filename = "{}.mp3".format(os.path.join(TRACK_CONTENT_PATH, track_title))
+#     print("Downloading {} and saving to {}".format(track['title'], filename))
+#     stream_url = api.get_stream_url(track['id'])
+#     response = urllib.request.urlopen(stream_url)
+#     data = response.read()
+#     with open(filename, "wb") as f:
+#         f.write(data)
+#     if 'storeId' in track:
+#         print("has store id : {}".format(track['title']))
+#     else:
+#         print("no store id : {}".format(track['title']))
 
 
 report["# Registered Devices"] = len(registered_devices)
